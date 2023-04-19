627bd45b23c563e946f9e275337df6bb69d86a09
Hieu Le <hieultit92@gmail.com>
2023-04-19 17:09:49 +0700

    Create dfs-stack.go
    dfs code generate from ChatGPT

---
+++ dfs-stack.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Graph struct {
+	vertices []*Vertex
+}
+
+type Vertex struct {
+	key      int
+	visited  bool
+	neightbors []*Vertex
+}
+
+func (g *Graph) AddVertex(key int) {
+	vertex := &Vertex{key: key}
+	g.vertices = append(g.vertices, vertex)
+}
+
+func (g *Graph) AddEdge(from, to int) {
+	v1 := g.getVertex(from)
+	v2 := g.getVertex(to)
+
+	v1.neightbors = append(v1.neightbors, v2)
+	v2.neightbors = append(v2.neightbors, v1)
+}
+
+func (g *Graph) getVertex(key int) *Vertex {
+	for i := 0; i < len(g.vertices); i++ {
+		if g.vertices[i].key == key {
+			return g.vertices[i]
+		}
+	}
+
+	return nil
+}
+
+func (g *Graph) dfs(start *Vertex) {
+	stack := []*Vertex{}
+	stack = append(stack, start)
+
+	for len(stack) > 0 {
+		// Pop vertex from stack
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if !node.visited {
+			node.visited = true
+			fmt.Printf("%d ", node.key)
+		}
+
+		// Add neighbors to stack
+		for i := 0; i < len(node.neightbors); i++ {
+			if !node.neightbors[i].visited {
+				stack = append(stack, node.neightbors[i])
+			}
+		}
+	}
+}
+
+func main() {
+	graph := &Graph{}
+	graph.AddVertex(1)
+	graph.AddVertex(2)
+	graph.AddVertex(3)
+	graph.AddVertex(4)
+	graph.AddVertex(5)
+
+	graph.AddEdge(1, 2)
+	graph.AddEdge(1, 3)
+	graph.AddEdge(2, 4)
+	graph.AddEdge(2, 5)
+
+	fmt.Println("Depth First Traversal (starting from vertex 1):")
+	graph.dfs(graph.getVertex(1))
+}
